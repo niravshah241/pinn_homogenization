d76439c76a9c8c739a97d87e8dac131b581276bd
niravshah241 <niravshahcolab@gmail.com>
2024-04-10 21:04:29 +0100

    2d time depepdent example added, plotting pending

--- 2d_time_dependent.py
+++ 2d_time_dependent.py
@@ -18,40 +18,41 @@ def true_sol(domain_points, alpha=alpha, beta=beta):
         beta * (domain_points[:, 2])).unsqueeze(1)
 
 # Create collocation points on domain (x, y) and in time (t)
-# (x, y, t) order 0 \leq x \leq 1, 0 \leq y \leq 1, 0 \leq t \leq 2
+# (x, y, t) order 0 \leq x \leq 1, 0 \leq y \leq 1, 0 \leq t \leq t_max
+t_max = 0.2
 num_points_domain = 1000
 domain_points = \
     torch.hstack((torch.rand(num_points_domain, 1),
                   torch.rand(num_points_domain, 1),
-                  torch.rand(num_points_domain, 1))).to(device=device).to(dtype=dtype)
+                  torch.rand(num_points_domain, 1) * t_max)).to(device=device).to(dtype=dtype)
 
 # Bottom boundary y = 0
 num_points_bottom_boundary = 100
 bottom_bc_points = \
     torch.hstack((torch.rand(num_points_bottom_boundary, 1),
                   torch.zeros(num_points_bottom_boundary, 1),
-                  torch.rand(num_points_bottom_boundary, 1))).to(device=device).to(dtype=dtype)
+                  torch.rand(num_points_bottom_boundary, 1) * t_max)).to(device=device).to(dtype=dtype)
 
 # Right boundary x = 1
 num_points_right_boundary = 100
 right_bc_points = \
     torch.hstack((torch.ones(num_points_right_boundary, 1),
                   torch.rand(num_points_right_boundary, 1),
-                  torch.rand(num_points_right_boundary, 1))).to(device=device).to(dtype=dtype)
+                  torch.rand(num_points_right_boundary, 1) * t_max)).to(device=device).to(dtype=dtype)
 
 # Top boundary y = 1
 num_points_top_boundary = 100
 top_bc_points = \
     torch.hstack((torch.rand(num_points_top_boundary, 1),
                   torch.ones(num_points_top_boundary, 1),
-                  torch.rand(num_points_top_boundary, 1))).to(device=device).to(dtype=dtype)
+                  torch.rand(num_points_top_boundary, 1) * t_max)).to(device=device).to(dtype=dtype)
 
 # Left boundary x = 0
 num_points_left_boundary = 100
 left_bc_points = \
     torch.hstack((torch.zeros(num_points_left_boundary, 1),
                   torch.rand(num_points_left_boundary, 1),
-                  torch.rand(num_points_left_boundary, 1))).to(device=device).to(dtype=dtype)
+                  torch.rand(num_points_left_boundary, 1) * t_max)).to(device=device).to(dtype=dtype)
 
 # Initial condition t = 0
 num_points_initial_condition = 100
@@ -70,7 +71,7 @@ bc_points = torch.vstack((bottom_bc_points,
 u_bc_val = true_sol(bc_points)
 
 points_range = torch.tensor([[0., 0., 0.],
-                             [1., 1., 2.]]).to(device=device).to(dtype=dtype)
+                             [1., 1., t_max]]).to(device=device).to(dtype=dtype)
 points_scaling_range = torch.tensor([[-1., -1., -1.],
                                      [1., 1., 1.]]).to(device=device).to(dtype=dtype)
 
@@ -100,11 +101,11 @@ ic_dataset = CustomDataset(ic_points, u_ic_val,
                            points_scaling_range,
                            u_val_scaling_range)
 
-domain_dataloader = torch.utils.data.DataLoader(domain_dataset, batch_size=num_points_domain, shuffle=True)
-bc_dataloader = torch.utils.data.DataLoader(bc_dataset, batch_size=bc_points.shape[0], shuffle=True)
-ic_dataloader = torch.utils.data.DataLoader(ic_dataset, batch_size=num_points_initial_condition, shuffle=True)
+domain_dataloader = torch.utils.data.DataLoader(domain_dataset, batch_size=num_points_domain, shuffle=False)
+bc_dataloader = torch.utils.data.DataLoader(bc_dataset, batch_size=bc_points.shape[0], shuffle=False)
+ic_dataloader = torch.utils.data.DataLoader(ic_dataset, batch_size=num_points_initial_condition, shuffle=False)
 
-net = ANN_net([3, 10, 10, 1], torch.tanh).to(device=device).to(dtype=dtype)
+net = ANN_net([3, 20, 20, 1], torch.tanh).to(device=device).to(dtype=dtype)
 
 '''
 for batch, args in enumerate(domain_dataloader):
@@ -129,6 +130,7 @@ def pde_loss(u_pred, input_args, output_args, input_scaling_range, output_scalin
     diffusion_coefficient = torch.tensor([0.1]).to(x_scaled.device)
     # TODO Verify that pde_loss backpropagation is not slowed by split of the output columns below,
     # like previously in customdataloder case
+    # TODO tested only when u_pred is scalar
     u_x = torch.autograd.grad(u_pred[:, 0].sum(), x_scaled, create_graph=True)[0].to(x_scaled.device)
     u_y = torch.autograd.grad(u_pred[:, 0].sum(), y_scaled, create_graph=True)[0].to(y_scaled.device)
     u_t = torch.autograd.grad(u_pred[:, 0].sum(), t_scaled, create_graph=True)[0].to(t_scaled.device)
@@ -138,13 +140,13 @@ def pde_loss(u_pred, input_args, output_args, input_scaling_range, output_scalin
     residual = ((output_range[1][0] - output_range[0][0]) * (input_scaling_range[1][2] - input_scaling_range[0][2])) / ((output_scaling_range[1][0] - output_scaling_range[0][0]) * (input_range[1][2] - input_range[0][2])) * u_t[0] - diffusion_coefficient * ((input_scaling_range[1][0] - input_scaling_range[0][0])**2 / (input_range[1][0] - input_range[0][0])**2 * (output_range[1][0] - output_range[0][0]) / (output_scaling_range[1][0] - output_scaling_range[0][0]) * u_xx[0] + (input_scaling_range[1][1] - input_scaling_range[0][1])**2 / (input_range[1][1] - input_range[0][1])**2 * (output_range[1][0] - output_range[0][0])/ (output_scaling_range[1][0] - output_scaling_range[0][0]) * u_yy[0]) - f_true
     return torch.mean(residual**2)
 
-max_epochs = 5000
+max_epochs = 3000
 total_loss_list = list()
 residual_loss_list = list()
 ic_loss_list = list()
 bc_loss_list = list()
 
-optimiser = torch.optim.Adam(net.parameters(), lr=1.e-4)
+optimiser = torch.optim.Adam(net.parameters(), lr=1.e-3)
 
 for epoch in range(max_epochs):
     start_time = time.process_time()
@@ -156,8 +158,6 @@ for epoch in range(max_epochs):
     ic_loss_list.append(loss_ic.item())
     bc_loss_list.append(loss_bc.item())
     residual_loss_list.append(loss_residual.item())
-    if (epoch + 1) % 100 == 0:
-        print(f"Epoch: {epoch+1}/{max_epochs}, Loss: {total_loss.item()}, Loss boundary: {loss_bc.item()}, Loss IC: {loss_ic.item()}, Loss residual: {loss_residual.item()}")
     if epoch == 0:
         min_loss = total_loss_list[epoch]
     else:
@@ -167,4 +167,28 @@ for epoch in range(max_epochs):
         else:
             pass
     end_time = time.process_time()
-    print(f"Epoch: {epoch+1}, Loss: {total_loss.item()}, Epoch time: {end_time - start_time}")
+    if (epoch + 1) % 10 == 0:
+        print(f"Epoch: {epoch+1}/{max_epochs}, Loss: {total_loss.item()}, Loss boundary: {loss_bc.item()}, Loss IC: {loss_ic.item()}, Loss residual: {loss_residual.item()}, Epoch time: {end_time - start_time}")
+
+num_test_points = 100
+t_step_test = t_max / 2 # Test time step
+
+x_test = torch.rand(num_test_points).unsqueeze(1).to(device=device)
+y_test = torch.rand(num_test_points).unsqueeze(1).to(device=device)
+t_test = torch.ones(num_test_points).unsqueeze(1).to(device=device) * t_step_test
+
+test_input = torch.hstack((x_test, y_test, t_test))
+test_input_scaled = bc_dataloader.dataset.scale_input(test_input)
+
+x_test_scaled = test_input_scaled[:, 0]
+y_test_scaled = test_input_scaled[:, 1]
+t_test_scaled = test_input_scaled[:, 2]
+
+with torch.no_grad():
+    u_test = bc_dataloader.dataset.reverse_scale_output(net([x_test_scaled, y_test_scaled, t_test_scaled]))
+
+u_true = true_sol(test_input)
+
+print(f"ANN pred: {u_test.T}")
+print(f"True: {u_true.T}")
+print(f"Error: {torch.max(abs(u_test - u_true))}")
